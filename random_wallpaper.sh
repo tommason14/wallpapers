a190a73b54d2d9de8adb6d8b5c8877e681d467d4
Tom Mason <tommason14@gmail.com>
2019-08-22 22:49:08 +1000

    now checks if random choice is already set

--- random_wallpaper.sh
+++ random_wallpaper.sh
@@ -3,5 +3,16 @@
 cd ~/Documents/repos/wallpapers
 wallpapers=($(ls *.{png,jpg}))
 range=${#wallpapers[@]}
-option=$(( $RANDOM % $range ))
-./make_wallpaper.sh ${wallpapers[$option]}
+
+get_choice(){
+  option=$(( $RANDOM % $range ))
+  # if random is already chosen, pick again
+  if grep -q "${wallpapers[$option]}" $HOME/.cache/wal/wal
+  then
+    option=$(get_choice)
+  fi
+  echo $option
+}
+
+choice=$(get_choice)
+./make_wallpaper.sh ${wallpapers[$choice]}
