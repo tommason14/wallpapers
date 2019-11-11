c9835e4b95983d1b7cfe3fcac31080132562ec0e
Tom Mason <tommason14@gmail.com>
2019-11-12 10:44:51 +1100

    pics

--- make_wallpaper.sh
+++ make_wallpaper.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
-if [[ $USER == "tommason" ]]
-then
-  wal -i "$1"
-else
-  FILE="$HOME/Documents/repos/wallpapers/$1"
-  wal -ni "$FILE"
-  osascript << END
-  tell application "Finder" to set desktop picture to POSIX file "$FILE"
+# if [[ $USER == "tommason" ]]
+# then
+  # wal -i "$1"
+# else
+FILE="$HOME/Documents/repos/wallpapers/$1"
+wal -ni "$FILE"
+osascript << END
+tell application "Finder" to set desktop picture to POSIX file "$FILE"
 END
-fi
+# fi
