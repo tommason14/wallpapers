282200c0711069d83d2b3c92ed8163f12c0a376a
tommason14 <tom.mason14@gmail.com>
2020-03-30 19:52:06 +1100

    spotify theming

--- make_wallpaper.sh
+++ make_wallpaper.sh
@@ -4,6 +4,8 @@ pic=$(basename "$1") # deal with lf passing in whole name
 FILE="$PWD/$pic"
 wal -i "$FILE"
 cp ~/.cache/wal/colors-atom-syntax $HOME/.atom/packages/wal-syntax/styles/colors.less
+cp $HOME/.cache/wal/spicetify_colours.ini $HOME/spicetify_data/Themes/pywal/color.ini
+sed -i '' 's/\#//' $HOME/spicetify_data/Themes/pywal/color.ini
 
 # osascript << END
 # tell application "Finder" to set desktop picture to POSIX file "$FILE"
