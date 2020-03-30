7d8b62eb17854030264f199e8089a053740c8aa6
tommason14 <tom.mason14@gmail.com>
2020-03-30 18:24:28 +1100

    copy colours for pywal into atom when running the script

--- make_wallpaper.sh
+++ make_wallpaper.sh
@@ -3,6 +3,7 @@
 pic=$(basename "$1") # deal with lf passing in whole name
 FILE="$PWD/$pic"
 wal -i "$FILE"
+cp ~/.cache/wal/colors-atom-syntax $HOME/.atom/packages/wal-syntax/styles/colors.less
 
 # osascript << END
 # tell application "Finder" to set desktop picture to POSIX file "$FILE"
