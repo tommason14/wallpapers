24f53b60727aa3bdeda677c083eb0dc46c185eae
Tom Mason <tommason14@gmail.com>
2020-03-30 21:17:49 +1100

    changed apply to update

--- make_wallpaper.sh
+++ make_wallpaper.sh
@@ -6,6 +6,7 @@ wal -i "$FILE"
 cp ~/.cache/wal/colors-atom-syntax $HOME/.atom/packages/wal-syntax/styles/colors.less
 cp $HOME/.cache/wal/spicetify_colours.ini $HOME/spicetify_data/Themes/pywal/color.ini
 sed -i '' 's/\#//' $HOME/spicetify_data/Themes/pywal/color.ini
+spicetify update
 
 # osascript << END
 # tell application "Finder" to set desktop picture to POSIX file "$FILE"
