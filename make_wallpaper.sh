6286f06a3dcc7caaa750765918438fb1dcd119b0
Tom Mason <tommason14@gmail.com>
2020-04-01 00:00:18 +1100

    clean up

--- make_wallpaper.sh
+++ make_wallpaper.sh
@@ -3,11 +3,11 @@
 pic=$(basename "$1") # deal with lf passing in whole name
 FILE="$PWD/$pic"
 wal -i "$FILE"
+# atom
 cp ~/.cache/wal/colors-atom-syntax $HOME/.atom/packages/wal-syntax/styles/colors.less
+# spotify
 cp $HOME/.cache/wal/spicetify_colours.ini $HOME/spicetify_data/Themes/pywal/color.ini
 sed -i '' 's/\#//' $HOME/spicetify_data/Themes/pywal/color.ini
 spicetify update
-
-# osascript << END
-# tell application "Finder" to set desktop picture to POSIX file "$FILE"
-# END
+# yabai
+pgrep -q yabai && launchctl kickstart -k "gui/${UID}/homebrew.mxcl.yabai"
