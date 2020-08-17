309ef1af1d3de4be71b601a2e32783f2d3327ae9
tommason14 <tom.mason14@gmail.com>
2020-08-17 22:35:42 +1000

    walls

--- make_wallpaper.sh
+++ make_wallpaper.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# pic=$(basename "$1") # deal with lf passing in whole name
-# FILE="$PWD/$pic"
-
 # pass in options to wal, such as -l for a light scheme, -s to skip
 # changing terminal colours etc...
 
@@ -19,7 +16,7 @@ command -v pywalfox > /dev/null && pywalfox update
 pgrep -q yabai && launchctl kickstart -k "gui/${UID}/homebrew.mxcl.yabai" &
 pgrep -q spacebar && launchctl kickstart -k "gui/${UID}/homebrew.mxcl.spacebar" &
 
-kitty @ set-colors --all --configured ~/.config/kitty/kitty.conf
+[[ $TERM == "xterm-kitty" ]] && kitty @ set-colors --all --configured ~/.config/kitty/kitty.conf
 
 # spacebar looks odd with white bg, so if -l passed in, write to some file that
 # it was, and switch the colours in spacebarrc
